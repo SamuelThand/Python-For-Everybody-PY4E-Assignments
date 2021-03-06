77ca9671687eb81175617e6a190e5757d5ce2459
SamuelThand <79167687+SamuelThand@users.noreply.github.com>
2021-03-06 16:16:48 +0100

    Update ex_12_04.py

--- ex_12_04.py
+++ ex_12_04.py
@@ -1,10 +1,12 @@
-#Assignment
-#Change the urllinks.py program to extract and count paragraph (p) tags from the
-#retrieved HTML document and display the count of the paragraphs as the output of your program.
-#Do not display the paragraph text, only count them.
-#Test your program on several small web pages as well as some larger web pages.
-#
-#Assignment
+'''
+Change the urllinks.py program to extract and count paragraph (p) tags from the
+retrieved HTML document and display the count of the paragraphs as the output of your program.
+Do not display the paragraph text, only count them.
+Test your program on several small web pages as well as some larger web pages.
+'''
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
