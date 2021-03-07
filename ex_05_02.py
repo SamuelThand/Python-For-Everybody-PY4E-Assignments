fdaaf74165badb3324d34e840b60dd9f31de0f06
SamuelThand <79167687+SamuelThand@users.noreply.github.com>
2021-03-07 16:15:54 +0100

    Update ex_05_02.py

--- ex_05_02.py
+++ ex_05_02.py
@@ -1,8 +1,7 @@
-#Assignment
-#   Write another program that prompts for a list of numbers as above and at the end
-#   prints out both the maximum and minimum of the numbers instead of the average.
-#
-#   Solution
+'''
+Write another program that prompts for a list of numbers as above and at the end
+prints out both the maximum and minimum of the numbers instead of the average.
+'''
 
 maximum=None
 minimum=None
