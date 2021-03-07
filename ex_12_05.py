4bb30e2f98228e56c37b77e3010b136f97a63590
SamuelThand <79167687+SamuelThand@users.noreply.github.com>
2021-03-07 16:31:55 +0100

    Update ex_12_05.py

--- ex_12_05.py
+++ ex_12_05.py
@@ -1,9 +1,7 @@
-#Assignment
-#
-#(Advanced) Change the socket program so that it only shows data after the headers
-#and a blank line have been received. Remember that recv receives characters (newlines and all), not lines.
-#
-#Solution
+'''
+(Advanced) Change the socket program so that it only shows data after the headers
+and a blank line have been received. Remember that recv receives characters (newlines and all), not lines.
+'''
 
 import socket
 
