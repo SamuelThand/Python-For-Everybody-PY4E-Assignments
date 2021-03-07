e2cf9648b6954818a9859f7344fa9845773011d5
SamuelThand <79167687+SamuelThand@users.noreply.github.com>
2021-03-07 16:31:30 +0100

    Update ex_12_03.py

--- ex_12_03.py
+++ ex_12_03.py
@@ -1,11 +1,9 @@
-#Assignment
-#
-#Exercise 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL,
-#(2) displaying up to 3000 characters, and (3) counting the overall number of characters in the document.
-#Don’t worry about the headers for this exercise, simply show the first 3000 characters
-#of the document contents.import urllib.request, urllib.parse, urllib.error
-#
-#Assignment
+'''
+Exercise 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL,
+(2) displaying up to 3000 characters, and (3) counting the overall number of characters in the document.
+Don’t worry about the headers for this exercise, simply show the first 3000 characters
+of the document contents.import urllib.request, urllib.parse, urllib.error
+'''
 
 import urllib.request, urllib.parse, urllib.error
 
