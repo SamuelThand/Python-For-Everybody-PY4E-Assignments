d295cce7f9616e66d28d48f8a4dac55465eb077f
SamuelThand <79167687+SamuelThand@users.noreply.github.com>
2021-03-07 16:23:29 +0100

    Update ex_09_03.py

--- ex_09_03.py
+++ ex_09_03.py
@@ -1,16 +1,15 @@
-#Assignment
-#Add code to the above program to figure out who has the most messages in the file.
-#After all the data has been read and the dictionary has been created, look
-#through the dictionary using a maximum loop (see Chapter 5: Maximum and minimum loops)
-#to find who has the most messages and print how many messages the person has.
-#
-#   Enter a file name: mbox-short.txt
-#   cwen@iupui.edu 5
-#
-#   Enter a file name: mbox.txt
-#   zqian@umich.edu 195
-#
-#   Solution
+'''
+Add code to the above program to figure out who has the most messages in the file.
+After all the data has been read and the dictionary has been created, look
+through the dictionary using a maximum loop (see Chapter 5: Maximum and minimum loops)
+to find who has the most messages and print how many messages the person has.
+
+   Enter a file name: mbox-short.txt
+   cwen@iupui.edu 5
+
+   Enter a file name: mbox.txt
+   zqian@umich.edu 195
+'''
 
 name = input('Enter file name: ')
 if len(name) < 1 : name = 'mbox-short.txt'
